d5d4bf363f0720e1f7ee461588c14d7add2de374
Myekman <my@traningstemplet.se>
2023-09-18 07:59:20 +0000

    navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello World</h1>
-     <Button variant="primary">Primary</Button>
+     {/* <Button variant="primary">Primary</Button> */}
+     <NavBar/>
     </div>
   );
 }
