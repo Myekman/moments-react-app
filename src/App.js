560d6bd9dd822f3d11f08a88ec660da79b058177
Myekman <my@traningstemplet.se>
2023-09-18 08:24:48 +0000

    custom css styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import styles from './App.module.css';
 // import Button from 'react-bootstrap/Button';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
+    <div className={styles.App}>
      {/* <Button variant="primary">Primary</Button> */}
      <NavBar/>
     </div>
